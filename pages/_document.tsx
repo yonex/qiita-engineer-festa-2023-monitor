0e03ae32b67480f5d54289cc4c7593e9dd6eedfa
yonex <4462818+yonex@users.noreply.github.com>
2023-07-26 13:38:54 +0900

    title

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,7 +3,9 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <title>Qiita Engineer Festa 2023 Monitor</title>
+      </Head>
       <body>
         <Main />
         <NextScript />
